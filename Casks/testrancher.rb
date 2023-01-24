48a4821e3aed50b8064fff1d69bcfc1f64571f1d
DanielSotoEstrada <dsoto@includedhealth.com>
2023-01-24 14:36:59 -0700

    version changed and sha

--- Casks/testrancher.rb
+++ Casks/testrancher.rb
@@ -1,6 +1,6 @@
 cask "testrancher" do
     version "1.7.0"
-    sha256 ":no_check"
+    sha256 "41feea152b3dcff8fb729106b195e4dc7632cda669ef7054045f72c595825242"
       
     url "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.7.0/Rancher.Desktop-1.7.0.aarch64.dmg"
     name "Rancher Desktop"
--- formula/testtree.rb
+++ formula/testtree.rb
@@ -1,5 +1,5 @@
 class Testtree < Formula
-    VERSION="v1.0.8"
+    VERSION="v1.0.9"
     desc "Brew formula for installing tree."
     homepage "https://github.com/DanielSotoEstrada/testbrewformula"
     license "CC BY-NC-ND 4.0"
