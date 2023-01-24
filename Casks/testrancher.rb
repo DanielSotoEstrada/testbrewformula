01b1cc48643629520b1b80c3a42baf64fca04c7d
DanielSotoEstrada <dsoto@includedhealth.com>
2023-01-24 14:46:26 -0700

    version changed an sha added

--- Casks/testrancher.rb
+++ Casks/testrancher.rb
@@ -2,7 +2,7 @@ cask "testrancher" do
     version "1.7.0"
     sha256 "41feea152b3dcff8fb729106b195e4dc7632cda669ef7054045f72c595825242"
       
-    url "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.7.0/Rancher.Desktop-1.7.0.aarch64.dmg"
+    url "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v#{version}/Rancher.Desktop-#{version}.aarch64.dmg"
     name "Rancher Desktop"
     desc "Alternative to Docker Desktop"
     homepage "https://rancherdesktop.io/"
