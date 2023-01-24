7e66d4c8c1ab80007f5b3c55042d56c78fd49919
DanielSotoEstrada <dsoto@includedhealth.com>
2023-01-24 10:22:33 -0700

    version 4

--- formula/testtree.rb
+++ formula/testtree.rb
@@ -1,5 +1,5 @@
 class Testtree < Formula
-    VERSION="v1.0.3"
+    VERSION="v1.0.4"
     desc "Brew formula for installing tree."
     homepage "https://github.com/DanielSotoEstrada/testbrewformula"
     license "CC BY-NC-ND 4.0"
