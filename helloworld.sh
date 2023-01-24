c85e80d97ae925b53276e635792daa4640b0d871
DanielSotoEstrada <dsoto@includedhealth.com>
2023-01-24 09:19:05 -0700

    changes to the bash script

--- helloworld.sh
+++ helloworld.sh
@@ -12,29 +12,29 @@ echo "$NAME likes to play ${SPORT}ball"
 
 # para inputs  indicamos -p (prompt) y al final indicamos la variable 
 # en donde se almacenara 
-read -p "Enter your name: " NAME2
-echo "Hi $NAME2"
+#read -p "Enter your name: " NAME2
+#echo "Hi $NAME2"
 
 # Multiples inputs
-echo "Enter your full name: "
-read FNAME LNAME
-echo "YOUR FULL NAME IS $FNAME and $LNAME"
+#echo "Enter your full name: "
+#read FNAME LNAME
+#echo "YOUR FULL NAME IS $FNAME and $LNAME"
 
 # En los ifs siempre hay que agregar un espacio (en lugar de ==) para indicar
 # que estamos comparando y no estamos asignando
 
 # if [ "$NAME"="JOHN"] <---- Debido a que no hay espacio 
 # estamos asignando el valor "JOHN"
-if [ "$FNAME" = "Luis" ];
-then
-    echo "Excellent Luis"
-    echo "we are going to install tree with brew"
-    brew tap DanielSotoEstrada/testbrewformula https://github.com/DanielSotoEstrada/testbrewformula.git
-    brew install testtree
-else
-    echo "You are not Luis"
-    echo "I cannot install tree"
-fi
+# if [ "$FNAME" = "Luis" ];
+# then
+#     echo "Excellent Luis"
+echo "we are going to install tree with brew"
+brew tap DanielSotoEstrada/testbrewformula https://github.com/DanielSotoEstrada/testbrewformula.git
+brew install testtree
+# else
+#     echo "You are not Luis"
+#     echo "I cannot install tree"
+# fi
 
 
 # For execution:
