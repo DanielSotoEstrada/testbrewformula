9227a471184d114593e332bcd0a5b29502fdd24b
DanielSotoEstrada <dsoto@includedhealth.com>
2023-03-22 10:04:30 -0700

    version upgraded

--- Casks/ih-rancher.rb
+++ Casks/ih-rancher.rb
@@ -1,18 +1,12 @@
 cask "ih-rancher" do
-    version "1.8.0"
-    #sha256 "41feea152b3dcff8fb729106b195e4dc7632cda669ef7054045f72c595825242"
-      
-    #url "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v#{version}/Rancher.Desktop-#{version}.aarch64.dmg"
-    #https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.7.0/Rancher.Desktop-1.7.0.x86_64.dmg
-    #https://docs.brew.sh/Formula-Cookbook#inside-def-install-and-test-do
+    version "1.8.1"
 
-    # Asi probar..si jala, solo conseguir el SHA del intel y hacer el Pull Request
     if Hardware::CPU.intel? 
         url "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v#{version}/Rancher.Desktop-#{version}.x86_64.dmg"
-        sha256 "005d53c75987b9878e62ce8f404a9d7163d2be1815fab38a4cfb8336402606ba"
+        sha256 "5233a6b7024077bb2102f8c4afde7bed79207ab4d1989d8b2e34e5b353ef3d82"
     else
         url "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v#{version}/Rancher.Desktop-#{version}.aarch64.dmg"
-        sha256 "8c1a6cf7e37020074236b49d6374c313870a0682f7df8046d2d62bae648a0464"
+        sha256 "ed88c10760cf5ff03bfcfa68870a90bb839d79dd95a8357def11875fb1ffbd23"
     end
     
     name "Rancher Desktop"
